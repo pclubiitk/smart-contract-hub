b2b1ec62cd5402a717e7de17f578521152cc88d8
adip1343 <aadhip43@gmail.com>
2021-07-18 13:32:07 +0530

    Added MMPR

---
+++ Voting/MMPR/MMPR.sol
@@ -0,0 +1,120 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract MMPR{
+    
+    address public chairperson;     // chairperson of the election
+    uint parties = 3;               // number of parties in the election
+    uint seats;                     // number of seats in the election
+    uint size;                      // size of parliment
+    
+    address[] provisionalVoters;    // provisional voters
+    uint ptr = 0;                   // pointer in provisionalVoters array
+    
+    uint totalVotes = 0;            // totalVotes casted
+    uint[3][] public firstVotes;    // first votes of voters
+    uint[3] public secondVotes;     // secondVotes of voters
+    
+    uint[3] public representation;  // representation of parties in parliment
+    
+    struct Voter {
+        bool voted;         // if true, that person already voted
+        bool autherized;    // if true, person is autherized to vote
+        uint seat;          // number of constituency
+        
+        uint firstVote;     // index of first vote
+        uint secondVote;    // index of second vote
+    }
+    
+    
+    mapping(address => Voter) public voters;    // mapping between address and voter
+    
+    // constructs smart contract with size = 2*seats
+    constructor (uint _seats) {
+        chairperson = msg.sender;
+        seats = _seats;
+        size = 2*seats;
+        for(uint i=0; i<seats; i++) {
+            firstVotes.push([0, 0, 0]);
+        }
+    }
+    
+    // these are added to provisionalVoters and then periodically autherized by chairperson
+    function requestRightToVote(uint seat) public {
+        address voter = msg.sender;
+        require(
+            !voters[voter].autherized,
+            "The voter already autherized."
+        );
+        require(
+            seat < seats,
+            "No such constituency exists."
+        );
+        voters[voter].seat = seat;
+        provisionalVoters.push(voter);
+    }
+    
+    // autherizing votes
+    function giveRightToVote() public {
+        require(
+            msg.sender == chairperson,
+            "Only chairperson can give right to vote."
+        );
+        
+        for(; ptr<provisionalVoters.length; ptr++){
+            if(!voters[provisionalVoters[ptr]].voted) {
+                voters[provisionalVoters[ptr]].autherized = true;
+            }
+        }
+    }
+
+    // casting votes
+    function castVote(uint first, uint second) public {
+        address voter = msg.sender;
+        
+        require(
+            voters[voter].autherized,
+            "Voter is not autherized"
+        );
+        
+        require(
+            !voters[voter].voted,
+            "Voter has already voted"
+        );
+        
+        require(
+            (first<parties) && (second<parties),
+            "Invalid Vote"
+        );
+        voters[voter].voted = true;
+        firstVotes[voters[voter].seat][first] += 1;
+        secondVotes[second] += 1;
+        totalVotes += 1;
+    }
+    
+    // finding final representation of candidates
+    function result() public {
+        for(uint i=0; i<seats; i++){
+            uint winner = 0;
+            uint max = 0;
+            
+            // from first votes
+            for(uint j=0; j<parties; j++){
+                if(firstVotes[i][j]>max){
+                    winner = j;
+                    max = firstVotes[i][j];
+                }
+            }
+            representation[winner] += 1;
+        }
+        
+        // from second voter
+        // giving representation to a party till it is overepresented in the parliment
+        uint occupied = seats;
+        for(uint i=0; i<parties; i++){
+            while(representation[i]*totalVotes <= secondVotes[i]*size  && occupied <= size){
+                representation[i] += 1;
+                occupied += 1;
+            }
+        }
+    }
+}
---
+++ Voting/MMPR/README.md
@@ -0,0 +1,28 @@
+# Mixed Member Proportional Representation(MMPR)
+
+In MMPR each voter casts two votes.
+
+1. *First Vote* : This is used to decide winner of each constituency by First Past The Post system
+2. *Second Vote* : This is used to decide representation of each political party in Parliament.  Here will allot extra seats to each party till it is over-represented and then move on to next political party.
+
+## Implementation
+
+1. `constructor` 
+   - creates the contract with required number of `seats` and initialises `size` of the Parliament as `2*seats`
+   - sets the `chairperson` of the contract.
+
+2. `requestRightToVote` 
+   - seat of voter as argument
+   - caller of this method are stored in `provisionalVoters` array if they haven't voted or autherized already.
+3. `giveRightToVote`
+   - `chairperson` can call this method to autherize all users in `provisionalList` to vote
+4. `castVote`
+   - each autherized voter can cast 2 votes
+5. `result`
+   - FIrst gives `seats` to party by FPTP in each seat.
+   - Remaining 50% seats are filled by allocating candidates to each party until it is over-represented.
+
+#### Contributors
+
+1. [Patil Aditya Bharat](https://github.com/adip1343)
+2. Rohit Kumar
