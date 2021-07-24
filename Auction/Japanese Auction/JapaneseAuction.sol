d57c45394493253cc448fc263a99f53420d04c9a
yushananda <84017927+yushananda@users.noreply.github.com>
2021-07-24 21:15:13 +0530

    Update JapaneseAuction.sol

--- Auction/Japanese Auction/JapaneseAuction.sol
+++ Auction/Japanese Auction/JapaneseAuction.sol
@@ -1,9 +1,9 @@
 pragma solidity >=0.7.0 <0.9.0;
 
-contract auction
+contract japaneseAuction
 {
     
-    struct buyer
+    struct buyer             // structure buyer contains thhe buyer's address, bool variables to check if they are authenticated and currently in the arena, and their index
     {
         address add;
         bool isAuth;
@@ -12,20 +12,21 @@ contract auction
     
     }
     
-    address auctioner;
-    uint256 highestBid;
+    address auctioner;     
     uint256 basePrice;
     bool isEnded;
     bool isStarted;
-    buyer [] buyerList;
     uint public numberOfbuyers;
-    event currentBid(uint amount);
-    event AuctionEnded(address winner, uint amount);
     uint256 initialTime;
     uint256 public incrementPerSec;
+    
+    buyer [] buyerList;
     mapping(address => buyer )buyerListMap;
     
-    modifier isAuctioner{
+    
+    event AuctionEnded(address winner, uint amount);
+    
+    modifier isAuctioner{                  // modifier to ensure certain functions can only called by the auctioneer
         require( auctioner == msg.sender, "Only Auctioner can call this");
         _;
     }
@@ -40,7 +41,7 @@ contract auction
         
     }
     
-    function setBasePrice(uint256 _basePrice , uint256 _incrementPerSec) isAuctioner public{
+    function setBasePrice(uint256 _basePrice , uint256 _incrementPerSec) isAuctioner public{    // function used by the auctioneer to set a base price
         basePrice = _basePrice;
         incrementPerSec = _incrementPerSec;
         
@@ -48,7 +49,7 @@ contract auction
     
     
     
-    function signUpAsBuyer() public
+    function signUpAsBuyer() public      // function that can be accessed publicly to sign up as a buyer
     {
         require( !buyerListMap[msg.sender].exists , " Already Exists " );
         buyerList.push(buyer(msg.sender , false , true , buyerList.length));
@@ -56,7 +57,7 @@ contract auction
         
     }
     
-    function authorise() isAuctioner public{
+    function authorise() isAuctioner public{     // used by the auctioneer to authorise buyers
         
         for(uint256 i=0; i<buyerList.length;++i){
             
@@ -72,8 +73,8 @@ contract auction
         
     }
     
-    function startAuction() isAuctioner public{
-        
+    function startAuction() isAuctioner public{       // auctioneer uses this function to start the auction
+        require( !isEnded , " The Auction has ended, again deploy the contract to start new Auction");
         require( !isStarted , "Auction Already Started");
         isStarted = true;
         initialTime = block.timestamp;
@@ -81,7 +82,8 @@ contract auction
     }
     
     
-    function getCurrentPrice() public view returns (uint256 ) {
+    function getCurrentPrice() public view returns (uint256 ) {       // returns the current price of the item
+    
         uint256 currPrice = basePrice;
         
         if(isStarted)
@@ -97,7 +99,7 @@ contract auction
         
     }
     
-    function endAuction() private
+    function endAuction() private      //  used to end the auction
     {
         require ( !isEnded , "Auction Already ended");
         require ( isStarted , "Auction not Started Yet");
@@ -107,14 +109,14 @@ contract auction
         emit AuctionEnded(buyerList[0].add , getCurrentPrice());
     }
     
-    function withdraw () public 
+    function withdraw () public        // a bidder may use this function to withdraw themselves from the auction, i.e. stop bidding
     {
         require( buyerListMap[msg.sender].exists , " Already withdrawn " );
-        
         require ( !isEnded , " Auction Already Ended");
         
         uint256 ind = buyerListMap[msg.sender].index ;
-        buyerList [ind] = buyerList[buyerList.length - 1];
+        buyerList[numberOfbuyers - 1].index = ind ;
+        buyerList [ind] = buyerList[numberOfbuyers - 1];
         buyerListMap[msg.sender].exists = false ;
         numberOfbuyers --;
         if(numberOfbuyers == 1){
